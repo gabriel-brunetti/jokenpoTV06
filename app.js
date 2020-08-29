524c4947a7738024e7d59dceb5ffff96937cc4f1
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2020-08-29 15:26:37 -0300

    switch feito e botao reset

--- app.js
+++ app.js
@@ -18,37 +18,73 @@ const scissorsButton_img = document.querySelector("#scissorsButton");
 // ARROW FUNCTION
 // não declarada, sintaxe:
 // () => {
-    // codigo funcao
+// codigo funcao
 // }
 // () => codigo funcao
 
-function jogo(cliqueDoUsuario){
-    jogadasPossiveisComputador = ["rock", "paper", "scissors"]
-    n = Math.floor(Math.random() * 3);
-    cliqueDoComptuador = jogadasPossiveisComputador[n];
+function jogo(cliqueDoUsuario) {
+  jogadasPossiveisComputador = ["rock", "paper", "scissors"];
+  n = Math.floor(Math.random() * 3);
+  cliqueDoComptuador = jogadasPossiveisComputador[n];
 
-    console.log(cliqueDoUsuario + cliqueDoComptuador)
-    switch(cliqueDoUsuario + cliqueDoComptuador) {
-        //vitória do usuário
-        
-
-        //derrota do usuário
-
-
-        //empate
-
-
-        //default para algum erro
-    }
-
+  // console.log(cliqueDoUsuario + cliqueDoComptuador)
+  switch (cliqueDoUsuario + cliqueDoComptuador) {
+    //vitória do usuário
+    case "paperButtonrock":
+    case "rockButtonscissors":
+    case "scissorsButtonpaper":
+      pontuacaoUsuario++;
+      userScore.innerHTML = pontuacaoUsuario;
+      result.innerHTML = "Parabéns você venceu";
+      break;
+    //derrota do usuário
+    case "paperButtonscissors":
+    case "rockButtonpaper":
+    case "scissorsButtonrock":
+      pontuacaoComputador++;
+      compScore.innerHTML = pontuacaoComputador;
+      result.innerHTML = "Você perdeu triste";
+      break;
+    //empate
+    case "paperButtonpaper":
+    case "rockButtonrock":
+    case "scissorsButtonscissors":
+      result.innerHTML = "Deu empate";
+      break;
+    //default para algum erro
+    default:
+      result.innerHTML = "Aconteceu algum erro jogue novamente";
+      break;
+  }
 }
-
-
 
 function principal() {
-    paperButton_img.addEventListener("click", () => jogo("paperButton") );
-    rockButton_img.addEventListener("click", () => jogo("rockButton") );
-    scissorsButton_img.addEventListener("click", () => jogo("scissorsButton") );
+  paperButton_img.addEventListener("click", () => jogo("paperButton"));
+  rockButton_img.addEventListener("click", () => jogo("rockButton"));
+  scissorsButton_img.addEventListener("click", () => jogo("scissorsButton"));
 }
 
+// Outra Possibilidade!
+// function resetar() {
+//   resetButton.addEventListener("click", () => {
+//     // resetando a variável que guarda o placar
+//     pontuacaoUsuario = 0;
+//     pontuacaoComputador = 0;
+//     // resetando o placar exibido no html
+//     userScore.innerHTML = 0;
+//     compScore.innerHTML = 0;
+//   });
+// }
+
+// // BOTÃO RESETAR
+resetButton.onclick = () => {
+  // resetando a variável que guarda o placar
+  pontuacaoUsuario = 0;
+  pontuacaoComputador = 0;
+  // resetando o placar exibido no html
+  userScore.innerHTML = 0;
+  compScore.innerHTML = 0;
+};
+
 principal();
+// resetar();
